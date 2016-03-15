69557f0d7bbb62aa1af1ca70201ad0133ad0d777
Emanuele Origgi <emanuele@origgi.com>
2016-03-15 23:15:59 +0100

    some basic examples

---
+++ helloworld.js
@@ -0,0 +1,29 @@
+//console.log("Ciao");
+
+/// Server ************************************
+//var http = require("http");
+
+//http.createServer(function (request, response) {
+//  response.writeHead(200, {'Content-Type': 'text/plain'});
+
+//  response.end('Ciao');
+//}).listen(8081);
+
+// console.log('Node Server is running');
+
+
+/// Read file *********************************
+// Blocking
+// var fs = require("fs");
+// var data = fs.readFileSync('testFile.txt');
+// console.log(data.toString());
+
+// Non-Blocking
+// var fs = require("fs");
+// fs.readFile('testFile.txt', function(err, data) {
+//   if(err)return console.error(err);
+//   console.log(data.toString());
+// });
+
+
+// 
